bd2b535455a5aedcf1c06c5835f800d447a1bf28
MuntahaHaider <muntahahaiderali222@gmail.com>
2024-02-23 11:00:40 +0500

    Assignment 14

---
+++ app.js
@@ -0,0 +1,181 @@
+                                  // chapter 01
+// Q1
+var greetingmessage = "Hello! Welcome to our website.";
+alert(greetingmessage);
+
+// Q2
+alert( "Error ! plz enter a valid password." )
+
+// Q3
+alert("Welcome to JS Land... \n Happy Codding")
+
+// Q4
+alert("Welcome to JS Land... ")
+alert("Happy Codding \n Prevent this page from creating aditional dialogs.")
+
+
+// Q5
+console.log("Hello.. I can run JS through my web browser's console.")
+
+// Q6
+// answer in html file
+
+// Q7
+// a) Work
+// b) not work 
+// c) work
+// d) no work
+
+
+                                  // chapter 02
+// Q1
+var username;
+console.log(username)
+
+// Q2
+var myname = "Muntaha Haider"
+console.log("my name is " + " " + myname)
+
+// Q3
+var message;
+message = "Hello World";
+alert(message);
+
+// Q4
+var name = "Jhon" ;
+var age = "15 years old" ;
+var develop = "Certified Mobile Applicatrion Development."
+alert(name)
+alert(age)
+alert(develop)
+
+// Q5
+var JS = "plz" , Jav = "plzz" , Java = "plzzz" , Javas = "plzzz" , Javass = "plzzz"
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+
+// Q6
+var email = " example@example.com " ;
+alert("My email Address is " + email)
+
+// Q7
+var book = "“A smarter way to learn JavaScript”."
+alert(book)
+
+// Q8
+document.write("Yeh! I can write HTML content throough JavaScript.")
+
+// Q9
+var border = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+document.write("<br> “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” <br>")
+alert(border)
+
+
+
+                                  // chapter 03
+
+
+// Q1
+var age = "19 years old"
+alert("I am " + age)
+
+// Q2
+var number1 = 10
+var number2 = 4
+var final = number1 + number2
+alert("You have visited this site " + final + " time.")
+
+// Q3
+var birthyear = 2004
+document.write( " My birth year is " + birthyear + "</br> Data type of my ddcleared variable is number. ")
+
+// Q4
+var Visitorsname = "John Doe" 
+var Producttitle = " T-shirt"
+var quantity = 5
+document.write( "<br>" + Visitorsname + " ordered " + quantity + Producttitle + " (s) on XYZ Clothing store.")
+
+
+
+                                  
+                                  
+                           // CHAPTER 1 - 3
+
+                            // CHAP 01
+// Q1
+var firstname = "abcd"
+var lastname = "efgh"
+var email = "abcdefgh@gmail.com" 
+var phone = 92123456789
+var password = "ijklmnop"
+alert(firstname)
+alert(lastname)
+alert(email)
+alert(phone)
+alert(password)
+
+
+
+// Q2
+// Correct this statement: alert "you're learning JavaScript!" ;
+alert("you're learning JavaScript!")
+
+// Q3
+// Code an alert statement displaying any message you like.
+alert(" <b> <i> This is my First Assignment of JavaScript </b> </i> ")
+
+
+                            // CHAP 02
+
+
+// Q1
+var MyVariableName = "Variable Value"
+
+// Q2
+var number ;
+number = "Hello, world!" ;
+
+
+// Q3
+var teamName = "Jawan Pakistan" ;
+alert(teamName)
+
+
+// Q4
+var bestMan = "My Father" ; 
+
+
+                             // CHAP 03
+
+
+// Q1
+var caseQty ;
+
+
+// Q2
+caseQty = 144;
+
+
+// Q3
+var num = 9;
+
+// Q4
+var num1 = 10 
+var num2 = 10 
+var total = num1 + num2 
+console.log(total)
+
+
+// Q6
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+            //    =    100     +    10
+            //    =         110
+
+
+// Q5
+var num ;
+num = 10
+var total = num + 5 
+console.log(total)
